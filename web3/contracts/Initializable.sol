3900d962185c0d495edf08295e9c9e395200804e
faisal <faisalfirdani01@gmail.com>
2023-10-26 17:38:32 +0700

    Defi Initialize Contract

---
+++ web3/contracts/Initializable.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+//IMPORTING CONTRACT
+import "./Address.sol";
+
+abstract contract Initializable {
+    
+    uint8 private _initialized;
+
+    bool private _initializing;
+
+    event Initialized(uint8 version);
+
+    modifier intializer() {
+        bool isTopLevelCall = !_initializing;
+        require(
+            (isTopLevelCall && _initialized < 1) || (!Address.isContract(address(this)) && _initialized == 1),
+            
+            "Initializable: contract is already initialized"
+        );
+
+        _initialized = 1;
+
+        if(isTopLevelCall) {
+            _initializing = true;
+        }
+
+        _;
+        if(isTopLevelCall) {
+            _initializing = false;
+            emit Initialized(1);
+        }
+    }
+
+    modifier reinitializer(uint8 version) {
+        require(!_initializing && _initialized < version, "Initializable: contract is already initialized");
+
+        _initialized = version;
+        _initializing = true;
+        _;
+        _initializing = false;
+        emit Initialized(version);
+    }
+
+    modifier onlyInitializing() {
+        require(_initializing, "Initializable: contract is not initalizing");
+        _;
+    }
+
+    function _disableInitializers() internal virtual {
+        require(!_initializing, "Initializable: contract is initalizing");
+        if(_initialized < type(uint8).max) {
+            _initialized = type(uint8).max;
+            emit Initialized(type(uint8).max);
+        }
+    }
+}
