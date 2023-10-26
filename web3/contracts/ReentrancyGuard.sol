b5d6a77620717df8a3bd4b2c3a843aed72f8ddcf
faisal <faisalfirdani01@gmail.com>
2023-10-26 18:13:07 +0700

    Create Custom ERC20 Token Contract

---
+++ web3/contracts/ReentrancyGuard.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+abstract contract ReentrancyGuard {
+    
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+    
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    modifier nonReentrant() {
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        _status = _ENTERED;
+
+        _;
+
+        _status = _NOT_ENTERED;
+    }
+
+}
